8fe9a8486f69f4dfefe41bc2e3ca0c3eef8b9873
Danny3ec <do4771b@gre.ac.uk>
2024-11-20 20:59:50 +0000

    delete: remove myfirstrepo

--- Learning_C/helloworld.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h> // we need this library to get access to the input and output
-int main()
-{
-    printf("Hello World\n"); // lets say hello, where it all began...
-    printf("Goodbye World\n"); // this seems fitting as the program will close after
-    return 0; //returns 0 to teh int of main() and terminate the progragclsls
